4957cb41c3b64478292c17c1f37da60948bf4a44
Yahyazz <yahyaz.zakaria.4@gmail.com>
2023-05-23 19:41:12 +0700

    add deployurl

--- deployData.js
+++ deployData.js
@@ -10,6 +10,6 @@ const studentName = 'Yahya Zakaria'; // TODO: replace this
 
 const studentId = 'FE5581372'; // TODO: replace this
 
-const NetlifyDeployUrl = 'TODO: replace me'; // TODO: replace this
+const NetlifyDeployUrl = 'https://ruangperpus44.netlify.app/'; // TODO: replace this
 
 module.exports = { studentName, studentId, NetlifyDeployUrl };
